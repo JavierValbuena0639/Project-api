91a767e3a4dd2177db20cf3d62983802bee1e398
javi-valbuena <javier@mi.com.co>
2024-04-28 18:10:42 -0500

    Last Upload 28-04-2024 18 10

--- Project-api/Program.cs
+++ Project-api/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Project_api.Context;
+using Project_api.Model;
+using Project_api.Services;
 using System.Buffers.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,31 @@ var builder = WebApplication.CreateBuilder(args);
 var ConnectionString = builder.Configuration.GetConnectionString("Connection");
 builder.Services.AddDbContext<DbProject>(options => options.UseSqlServer(ConnectionString));
 builder.Services.AddControllers();
+
+#region AppRepo
+
+builder.Services.AddScoped<IClientsRepository, ClientService> ();
+builder.Services.AddScoped<IDetailInvoicesRepository, DetailInvoiceService>();
+builder.Services.AddScoped<IGamesRepository, GameService>();
+builder.Services.AddScoped<IInvoicesRepository, InvoiceService>();
+builder.Services.AddScoped<IProductionsRepository, ProductionService>();
+builder.Services.AddScoped<IProductTypesRepository, ProductTypeService>();
+builder.Services.AddScoped<IStoresRepository, StoreService>();
+builder.Services.AddScoped<IUsersRepository, UserService>();
+
+#endregion
+
+#region AppService
+builder.Services.AddScoped<ClientService>();
+builder.Services.AddScoped<DetailInvoiceService>();
+builder.Services.AddScoped<GameService>();
+builder.Services.AddScoped<InvoiceService>();
+builder.Services.AddScoped<ProductionService>();
+builder.Services.AddScoped<ProductTypeService>();
+builder.Services.AddScoped<StoreService>();
+builder.Services.AddScoped<UserService>();
+#endregion
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Project-api
 
 Without Controllers 
+Validate Program have diferents erros
